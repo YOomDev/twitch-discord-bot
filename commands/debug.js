ccc8bf5bdb2657fc85807b59751d15726f02d114
YOomDev <ype.oomes@gmail.com>
2024-09-17 21:32:36 +0200

    Start of refactor to make user experience better

---
+++ commands/debug.js
@@ -0,0 +1,13 @@
+const { SlashCommandBuilder } = require('discord.js');
+
+const debug = false;
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('debug')
+        .setDescription('Gives some debug info to the developer when it is enabled'),
+    async executeDiscord(interaction) {
+        if (debug) { interaction.client.utils.loggers.data(interaction); }
+        await interaction.reply(interaction.client.utils.buildEmbed("Debug", debug ? "Debug data has been given to devs." : "Debugging is currently turned off.", [], debug ? interaction.client.utils.color : interaction.client.utils.color_error));
+    },
+};
