e6ea644afcee6246c1b0c8a3dad824044bb3d709
YOomDev <ype.oomes@gmail.com>
2024-11-15 02:51:46 +0100

    Fixed command name for usage

--- commands/lurk.mjs
+++ commands/lurk.mjs
@@ -3,7 +3,7 @@ import { logData } from "../discord-bot-base/utils.mjs";
 
 export default {
     data: new SlashCommandBuilder()
-        .setName('debug')
+        .setName('lurk')
         .setDescription('Gives some debug info to the developer when it is enabled.'),
     async execute(interaction) {
         await interaction.reply(interaction.client.utils.buildEmbed("Error", "Lurking is not possible on discord?", [], interaction.client.utils.color_error));
