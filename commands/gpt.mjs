323e465cb0fbf49113d11aacb5cabff577424714
YOomDev <ype.oomes@gmail.com>
2024-11-19 19:35:17 +0100

    Fix GPT reply failing due to discord interaction timeout and failed GPT response parsing

--- commands/gpt.mjs
+++ commands/gpt.mjs
@@ -1,27 +1,22 @@
 import { SlashCommandBuilder } from 'discord.js';
 
 import { generateResponse, ROLES } from '../discord-bot-base/gpt.mjs';
-import { logInfo } from "../discord-bot-base/utils.mjs";
+import {logData, logError, logInfo} from "../discord-bot-base/utils.mjs";
 
 export default {
     data: new SlashCommandBuilder()
         .setName('gpt')
         .setDescription('not created yet'),
     async execute(interaction) {
-
-        interaction.reply("this command is not working yet!"); // TODO: TMP
-
-        // TODO: get input from user (this currently breaks!)
-        logInfo(`Message: ${interaction.content}`);
+        interaction.reply("Generating a response...");
 
         const response = await generateResponse(); // TODO: pass prompt to the gpt
         if (!response.error) {
             // TODO: add info to the conversation list
 
-            await interaction.reply(response.message.content);
+            await interaction.editReply(response.message.content);
             return;
         }
-
-        await interaction.reply(response.error);
+        await interaction.editReply(response.error);
     },
 };
