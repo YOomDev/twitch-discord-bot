f23b614bc5852cac412b090e10fd423eb97d36df
YOomDev <ype.oomes@gmail.com>
2025-02-03 20:50:31 +0100

    Remove unused imports and format code a bit nicer

--- commands/gpt.mjs
+++ commands/gpt.mjs
@@ -1,19 +1,20 @@
 import {SlashCommandBuilder, SlashCommandStringOption} from 'discord.js';
 
 import { generateResponse, ROLES } from '../discord-bot-base/gpt.mjs';
-import {logData, logError, logInfo} from "../discord-bot-base/utils.mjs";
 
 export default {
     data: new SlashCommandBuilder()
         .setName('gpt')
         .setDescription('Command to prompt a GPT')
-        .addStringOption(option => option.setRequired(true).setName('prompt').setDescription("The prompt you want to give to the gpt.")),
+        .addStringOption(option => option
+            .setName('prompt')
+            .setDescription("The prompt you want to give to the gpt.")
+            .setRequired(true)),
     async execute(interaction) {
         interaction.reply("Generating a response...");
 
-        const prompt = interaction.options.getString('prompt');
-
-        const response = await generateResponse([{ role: ROLES.SYSTEM, content: "Please answer the next question as short and concise as possible:" },{ role: ROLES.USER, content: prompt }]);
+        // Start generating response
+        const response = await generateResponse([{ role: ROLES.SYSTEM, content: "Please answer the next question as short and concise as possible:" },{ role: ROLES.USER, content: interaction.options.getString('prompt') }]);
         if (!response.error) {
             // TODO: add info to the conversation list
 
