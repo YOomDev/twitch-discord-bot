0d59dd8a36317d65757e399474b302e5d6d8369c
YOomDev <ype.oomes@gmail.com>
2025-02-03 20:50:04 +0100

    Make command arguments required

--- commands/alarm.mjs
+++ commands/alarm.mjs
@@ -7,10 +7,12 @@ export default {
         .setDescription('Sets a alarm of a specified amount of minutes')
         .addStringOption( option => option
             .setName('name')
-            .setDescription('The name of the timer.'))
+            .setDescription('The name of the timer.')
+            .setRequired(true))
         .addNumberOption(option => option
             .setName("minutes")
-            .setDescription("The amount of minutes the timer should be running for.")),
+            .setDescription("The amount of minutes the timer should be running for.")
+            .setRequired(true)),
     async execute(interaction) {
         let time = 0; // TODO: get input from message
         let name = ""; // TODO: get input from message
