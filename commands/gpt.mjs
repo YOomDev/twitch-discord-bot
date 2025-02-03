f775c4c3331c19705100bc7f1bb9cec74b162bbc
YOomDev <ype.oomes@gmail.com>
2025-02-03 19:27:15 +0100

    Make command work with required command argument

--- commands/gpt.mjs
+++ commands/gpt.mjs
@@ -1,4 +1,4 @@
-import { SlashCommandBuilder } from 'discord.js';
+import {SlashCommandBuilder, SlashCommandStringOption} from 'discord.js';
 
 import { generateResponse, ROLES } from '../discord-bot-base/gpt.mjs';
 import {logData, logError, logInfo} from "../discord-bot-base/utils.mjs";
@@ -7,11 +7,13 @@ export default {
     data: new SlashCommandBuilder()
         .setName('gpt')
         .setDescription('Command to prompt a GPT')
-        .addStringOption(option => { option.setRequired(true).setName('prompt').setDescription("The prompt you want to give to the gpt.") }),
+        .addStringOption(option => option.setRequired(true).setName('prompt').setDescription("The prompt you want to give to the gpt.")),
     async execute(interaction) {
         interaction.reply("Generating a response...");
 
-        const response = await generateResponse(); // TODO: pass prompt to the gpt
+        const prompt = interaction.options.getString('prompt');
+
+        const response = await generateResponse([{ role: ROLES.SYSTEM, content: "Please answer the next question as short and concise as possible:" },{ role: ROLES.USER, content: prompt }]);
         if (!response.error) {
             // TODO: add info to the conversation list
 
