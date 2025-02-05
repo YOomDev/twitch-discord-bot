fec626a630571f69176480c7cfe2da55983b7504
YOomDev <ype.oomes@gmail.com>
2025-02-05 23:38:11 +0100

    Make bots have a global that other bots are passed into for bot interactivity

--- bots.js
+++ bots.js
@@ -4,14 +4,15 @@ import { logError } from "./discord-bot-base/utils.mjs";
 import { start as discord } from "./discord-bot-base/bot.mjs";
 import { start as twitch } from "./twitch-bot-base/bot.mjs";
 
-const global = {};
+async function start() {
+    const disc = await startDiscord().catch(_ => {});
+    const ttv = await startTwitch().catch(_ => {});
 
-function start() {
-    startDiscord().catch(_ => {});
-    startTwitch().catch(_ => {});
+    disc.global.twitch = ttv;
+    ttv.global.discord = disc;
 }
 
-async function startDiscord() { discord().catch(err => logError(err)); }
-async function startTwitch() { twitch().catch(err => logError(err)); }
+async function startDiscord() { return discord().catch(err => logError(err)); }
+async function startTwitch() { return twitch().catch(err => logError(err)); }
 
 start();
