f0efb65dbb6bbc255e7abcc79d2ab8e1880b2bba
YOomDev <ype.oomes@gmail.com>
2025-02-21 14:03:37 +0100

    Added command functionality

--- commands/alarm.mjs
+++ commands/alarm.mjs
@@ -5,7 +5,19 @@ export default {
     // Twitch
     name: "alarm",
     async reply(client, channel, userState, params, message) {
-        // code...
+        if (params.length < 2) { client.utils.sendChannelMessage(channel, NO_ARGS); return; }
+        const name = params[0];
+        const number = parseInt(params[1]);
+        if (isNaN(number)) { client.utils.sendChannelMessage(channel, "Second argument is not a number!"); return; }
+        const total = Math.max(number, 0.017); // Clamp the timer to anything above one second
+        const hours = Math.floor(total / 60);
+        const minutes = Math.floor(total - (hours * 60));
+        const seconds = Math.floor((total - (hours * 60 + minutes)) * 60);
+        let time = hours > 0 ? `${hours} hour${hours > 1 ? "s" : ""}`.toString() : "";
+        if (minutes > 0) { time += `${time.length > 0 ? " and " : ""}${minutes} minute${minutes > 1 ? "s" : ""}`.toString(); }
+        if (seconds > 0) { time += `${time.length > 0 ? " and " : ""}${seconds} second${seconds > 1 ? "s" : ""}`.toString(); }
+        client.utils.sendChannelMessage(channel, `Timer \'${name}\' started for ${time}.`);
+        sleep(60 * total).then(_ => { client.utils.sendChannelMessage(channel, `Timer \'${name}\' ended.`); playAudio(`${name}.mp3`).catch(err => logError(err)) });
     },
 
     // Discord
