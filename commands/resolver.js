3e9533675478915e1a6db0dbd8d44fa3e132e23c
YOomDev <ype.oomes@gmail.com>
2025-02-21 13:47:56 +0100

    Move resolver out since it went unused in the bot base nad is only needed by certain current commands

---
+++ commands/resolver.js
@@ -0,0 +1,38 @@
+//////////////
+// Resolver //
+//////////////
+
+let lastRequestId = -1;
+const requests = [];
+
+export function createRequest() {
+    lastRequestId += 1;
+    const id = lastRequestId;
+    requests.push({ id: id, resolved: false, data: 0 });
+    return id;
+}
+
+export async function getSolvedRequest(id){
+    for (let i = 0; i < requests.length; i++) {
+        if (requests[i].id !== id) { continue; }
+        while (true) {
+            await sleep(0.5);
+            if (requests[i].resolved) {
+                const returnData = requests[i].data;
+                requests.splice(i, 1);
+                return returnData;
+            }
+        }
+    }
+    return 0;
+}
+
+export function resolveRequest(id, data) {
+    for (let i = 0; i < requests.length; i++) {
+        if (requests[i].id === id) {
+            requests[i].data = data;
+            requests[i].resolved = true;
+            return;
+        }
+    }
+}
