91acd3326552740a5d6432301da75a09e944532f
YOomDev <ype.oomes@gmail.com>
2025-02-23 19:52:53 +0100

    changed imported function names so they can be differentiated based on purpose

--- commands/debug.mjs
+++ commands/debug.mjs
@@ -1,6 +1,6 @@
 import { SlashCommandBuilder } from 'discord.js';
-import { logData } from "../discord-bot-base/utils.mjs";
-import { logInfo } from "../twitch-bot-base/utils.mjs";
+import { logData as logDataDiscord } from "../discord-bot-base/utils.mjs";
+import { logInfo, logData as logDataTwitch } from "../twitch-bot-base/utils.mjs";
 const debug = false;
 
 export default {
@@ -10,11 +10,11 @@ export default {
         if (debug) {
             logInfo(`Debug data for ${userState['display-name']} in ${channel}:`);
             logInfo("Message:");
-            logData(message);
+            logDataTwitch(message);
             logInfo("Params:");
-            logData(params);
+            logDataTwitch(params);
             logInfo("Userstate:");
-            logData(userState);
+            logDataTwitch(userState);
         }
         client.utils.sendChannelMessage(channel, debug ? "Debug data has been given to devs." : "Debugging is currently turned off.");
     },
@@ -24,7 +24,7 @@ export default {
         .setName('debug')
         .setDescription('Gives some debug info to the developer when it is enabled.'),
     async execute(interaction) {
-        if (debug) { logData(interaction); }
+        if (debug) { logDataDiscord(interaction); }
         await interaction.reply(interaction.client.utils.buildEmbed("Debug", debug ? "Debug data has been given to devs." : "Debugging is currently turned off.", [], debug ? interaction.client.utils.color : interaction.client.utils.color_error));
     },
 };
