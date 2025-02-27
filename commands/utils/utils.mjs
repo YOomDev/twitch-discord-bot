00396a4b059838371e16b7c35215b86587c23fbf
YOomDev <ype.oomes@gmail.com>
2025-02-27 13:41:26 +0100

    Moved util function to more local file

--- commands/utils/utils.mjs
+++ commands/utils/utils.mjs
@@ -1,3 +1,10 @@
 export async function sleep(seconds) { return new Promise(resolve => setTimeout(resolve, Math.max(seconds, 0) * 1000)); }
 
+export function concat(list, separator = "", prefix = "", start = 0, count = list.length) {
+    const end = Math.min(start + count, list.length);
+    let result = "";
+    for (let i = start; i < end; i++) { result += (i <= start ? "" : separator) + prefix + list[i]; }
+    return result;
+}
+
 export default {}
