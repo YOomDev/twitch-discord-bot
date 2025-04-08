4ef73897d3973fd88ec44c1093ecc0a9fb68e1e2
YOomDev <ype.oomes@gmail.com>
2025-04-08 23:45:23 +0200

    Added followage command parameter for checking other peoples followage

--- commands/followage.mjs
+++ commands/followage.mjs
@@ -5,7 +5,14 @@ export default {
     // Twitch
     name: 'followage',
     async reply(client, channel, userState, params, message) {
+        if (params.length > 0) {
+            if (params[0].length > 0) {
+                const follower = client.utils.isFollower(params[0], 'name');
+                client.utils.sendChannelMessage(channel, follower < 0 ? `${client.utils.getFollowerName(follower)} has not followed long enough to be checked.` : `${client.utils.getFollowerName(follower)} has followed for ${getTimeDifference(client.utils.getFollowerTime(follower))}.`);
+                return;
+            }
+        }
         const follower = client.utils.isFollower(userState['user-id']);
-        client.utils.sendChannelMessage(channel, follower < 0 ? 'You have not followed long enough to check' : getTimeDifference(client.utils.getFollowerTime(follower)));
+        client.utils.sendChannelMessage(channel, follower < 0 ? 'You have not followed long enough to check.' : `You have followed for ${getTimeDifference(client.utils.getFollowerTime(follower))}.`);
     },
 };
